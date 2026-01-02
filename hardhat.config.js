9622d82ddadacc4d904822750974d7b4977200c4
roua255 <rouabenjemaa10@gmail.com>
2026-01-02 22:32:22 +0100

    hardhat.js modifier

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,44 @@
+
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  
+  solidity: {
+    version: "0.8.20",  
+    settings: {
+      optimizer: {
+        enabled: true,   
+        runs: 200        
+      }
+    }
+  },
+  
+  
+  networks: {
+  
+    hardhat: {
+      chainId: 31337,
+     
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        accountsBalance: "10000000000000000000000" 
+      }
+    },
+    
+    
+    sepolia: {
+      url: "https://rpc.sepolia.org",
+      accounts: {
+        mnemonic: "votre phrase seed"
+      }
+    }
+  },
+  
+  
+  mocha: {
+    timeout: 120000  
+  },
+  
+
+
 };
